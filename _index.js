ff0a5a787ce4e4cb7028805f6c3f2c9e740436e3
travis <travis>
2016-10-07 13:37:16 +0000

    67cac400e0be011b455df4d25608870327d49878

--- _index.js
+++ _index.js
@@ -6289,6 +6289,29 @@ provide(
 
 });
 
+/** @class tabs */
+modules.define('tabs', ['i-bem__dom'], function(provide, BEMDOM) {
+    provide(BEMDOM.decl(this.name, /** @lends tabs.prototype */{}, /** @lends tabs */{
+        live : function() {
+            this.liveBindTo('switch', 'click', function(e) {
+                var prevIndex,
+                    newIndex;
+
+                if(!this.hasMod(e.currentTarget, 'active')) {
+                    prevIndex = this.elem('switch').index(this.elem('switch', 'active', true));
+                    newIndex = this.elem('switch').index(e.currentTarget);
+
+                    this.delMod(this.elem('switch', 'active', true), 'active');
+                    this.setMod(e.currentTarget, 'active', true);
+
+                    this.delMod(this.elem('tab').eq(prevIndex), 'active');
+                    this.setMod(this.elem('tab').eq(newIndex), 'active', true);
+                }
+            });
+        }
+    }));
+});
+
 (function(g) {
   var __bem_xjst = function(exports) {
      var $$mode = "", $$block = "", $$elem = "", $$elemMods = null, $$mods = null;
